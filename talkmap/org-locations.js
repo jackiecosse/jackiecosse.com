65cb530468bc68403a6e3fba1f4d2d0fa7294101
Jackie Cosse <105561988+jackiecosse@users.noreply.github.com>
2026-08-07 20:34:46 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,6 +1,6 @@
 var addressPoints = [
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
+    "Presentation 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
     51.4893335,
     -0.14405508452768728
   ],
@@ -10,12 +10,12 @@ var addressPoints = [
     -122.272863
   ],
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
+    "Conference Proceeding Presentation 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
     34.0536909,
     -118.242766
   ],
   [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
+    "Presentation 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
     37.7792588,
     -122.4193286
   ]
